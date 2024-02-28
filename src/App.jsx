6c0820e4cbeebdf20e3ee9b7c1273da2e3bda72d
priest davos <rajjo1920london@gmail.com>
2024-02-28 11:05:15 +0530

    1st commit on this branh(covered till 35)  (created useRef state so removed onChange listener as no need)  removed todoname and date useState  created ToDoelement and dateElement  used ref attribute in input tag

--- src/App.jsx
+++ src/App.jsx
@@ -9,54 +9,31 @@ import { useRef, useState } from "react"
 
 
 function App() {
-  const [todoName, setToDoName] = useState("")//temporory
-  const [date, setDate] = useState() //temporary
 
   const [todoItems, setToDoItems] = useState([]) //main list
   const [deletedTodoItems, setDeletedToDoItems] = useState([])//main deleted todo list
 
-  const [clickedAddBtn, setClickedAddBtn] = useState("false")//flag 
+  const ToDoNameElement = useRef()
+  const dateElement = useRef()
 
-  const numberOfNameUpdate=useRef(0)
-
-
-  //assign entered item/toDo to  toDoName variable //for input field
-  const handleOnChangeItem = (event) => {
-    numberOfNameUpdate.current +=1//printing it when date change
-    setClickedAddBtn("false");
-    setToDoName(event.target.value);
-    // console.log(event.target.value)   
-  }
-
-  // assign entered date to  date variable  //for input field
-  const handleOnChangeDate = (event) => {
-    console.log(`nuber of time name update ${numberOfNameUpdate.current}`)
-    setDate(event.target.value);
-    //  console.log(event.target.value)
-  }
 
   //Add  the entered itemm to toDolist 
   const handleOnClickAdd = (event) => {
-    //console.log(event);  // here event has attribute which has form input data
-    event.preventDefault();
-    setClickedAddBtn("true");
-    let newTodoItems = [...todoItems, { name: todoName, date: date }];
-    // setToDoItems(newTodoItems) 
-    // (todoName.length != 0) ? setToDoItems(newTodoItems) : null;
-    //  setToDoName("");
-    // // console.log(todoItem)
-    if (todoName.length !== 0) {
+    event.preventDefault();//important
+    const todoName = ToDoNameElement.current.value
+    const date = dateElement.current.value
+    //console.log(event);  // here event has attribute which has form input data but didnt use that way sinve used reference
+    if (todoName.length > 0) {
       let newTodoItems = [...todoItems, { name: todoName, date: date }];
       setToDoItems(newTodoItems);
-      setToDoName("");
+      ToDoNameElement.current.value = ""
+      //  dateElement.current.value=""
     }
-
   }
 
   //delete the selected item /todo from listy when  btn clicked
   const handleOnClickDelete = (event, item) => {
-  //console.log(event)
-    console.log(item)
+    //console.log(event)
     if (todoItems.includes(item)) {
 
       let newItems = [...todoItems] //create new item list
@@ -75,7 +52,7 @@ function App() {
     <center className="todo-container">
 
       <AppName />{/* comp 1 */}
-      <AddTodo handleOnChangeItem={handleOnChangeItem} handleOnChangeDate={handleOnChangeDate} handleOnClickAdd={handleOnClickAdd} clickedAddBtn={clickedAddBtn} />{/* comp 2 */}
+      <AddTodo handleOnClickAdd={handleOnClickAdd} ToDoNameElement={ToDoNameElement} dateElement={dateElement} />{/* comp 2 */}
       {/*  <Heading text={"ToDo  List"}/> */}
       <TodoItems todoItems={todoItems} handleOnClickDelete={handleOnClickDelete} showDelBtn={"true"} />
       { /*  <Heading text={"Completed ToDo List"}/> */}
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -2,23 +2,32 @@ import { useState } from "react";
 
 import { IoIosAddCircleOutline } from "react-icons/io";
 
-function AddTodo({ handleOnChangeItem, handleOnChangeDate, handleOnClickAdd, clickedAddBtn }) {
-  let [value, setValue] = useState("")
-  // console.log(clickedAddBtn)
+function AddTodo({ handleOnClickAdd,  ToDoNameElement, dateElement }) {
+  
+
   return (
     <div className="container text-center">
 
       <form onSubmit={handleOnClickAdd}>
-      
+
         <div className="row rows">
           <div className="col-7">
-            <input type="text" placeholder="Enter the todo here" onChange={(event) => { handleOnChangeItem(event); setValue(event.target.value); }} value={clickedAddBtn == "false" ? value : ""}></input>
+            <input
+              type="text"
+              ref={ToDoNameElement}
+              placeholder="Enter the todo here"
+             >
+            </input>
           </div>
           <div className="col-3">
-            <input type="date" onChange={(event) => { handleOnChangeDate(event) }}></input>
+            <input
+              type="date"
+              ref={dateElement}
+             >
+            </input>
           </div>
           <div className="col-2">
-            <button  className="btn btn-success  btn-sm  add-btn" ><IoIosAddCircleOutline /></button>
+            <button className="btn btn-success  btn-sm  add-btn" ><IoIosAddCircleOutline /></button>
           </div>
         </div>
       </form>
