26a855b9497ce5f9a0185be0745f31985ffb3ddb
priest davos <rajjo1920london@gmail.com>
2024-03-06 07:31:22 +0530

    restucting app.jsx file moving both pure func to store file create a component in store and move app.jsx func coxcept component here and in app.jsx use that compoonent and pass other sub /child comonent like appname, etc as children anduse thatchildren in store compont  returns TodoItemContextProvider
    --->Also removedDeleteToDoItem context aqs passddeleted todo ittem value
    in values of toDoItem context

--- src/App.jsx
+++ src/App.jsx
@@ -6,103 +6,33 @@ import TodoItemsContainer from "./components/TodoItems_container";
 
 import Heading from "./components/Heading";
 import { useReducer, useRef, useState } from "react";
-import { TodoItemsContext } from "./store/todo-items-store";
+import TodoItemContextProvider, { TodoItemsContext } from "./store/todo-items-store";
 import { DeletedTodoItemsContext } from "./store/deleted-todo-iems-store";
 
-let i = 0
-const todoItemsReducer = (currentTodoItems, action) => {
-  //param 1 of todoItems useReducer
-  let newTodoItems = currentTodoItems;
 
-  if (action.type == "NEW_ITEM") {
-    newTodoItems = [
-      ...currentTodoItems,
-      { name: action.payload.todoName, date: action.payload.date },
-    ];
-  } else if (action.type == "DEL_ITEM") {
-    let item = action.payload.item
-    // console.log(item)
-    if (newTodoItems.includes(item)) {
-      let newItems = [...newTodoItems]; //create new item list
-      let indexOfSelectedItem = newTodoItems.indexOf(item); //get index of selected item
-      let removed_item = newItems.splice(indexOfSelectedItem, 1); //remove the selected item from list and return it
-      // console.log(removed_item[0])
-
-      newTodoItems = newItems
-      console.log(i += 1)
-      action.payload.dispatchDeletedTodoIems({ type: "ADD_ITEM", item: removed_item[0] })
-    }
-
-  }
-
-  return newTodoItems;
-};
-const deletedTodoItemsReducer = (currentState, action) => {
-  let deletedTodoItems = currentState;
-  console.log(deletedTodoItems)
-  if (action.type === "ADD_ITEM") {
-
-    if (!currentState.includes(action.item)) {
-      deletedTodoItems = [...currentState, action.item]
-    }
-  }
-
-  return deletedTodoItems
-
-}
 
 function App() {
 
-  const [todoItems, dispatchTodoIems] = useReducer(todoItemsReducer, []);
-  const [deletedTodoItems, dispatchDeletedTodoIems] = useReducer(deletedTodoItemsReducer, []); //main deleted todo list
-
-  //updaing todoItems main list when click add
-  const onClickAdd = (todoName, date) => {
-    //creating obj for action
-    const newItemAction = {
-      type: "NEW_ITEM",
-      payload: {
-        todoName,
-        date,
-      },
-    };
-    // calling finc which we get from UseReducer whichtaking obj as arg
-    dispatchTodoIems(newItemAction);
-  };
-
-  const handleOnClickDelete = (event, item) => {
-    const DeleteItemAction = {
-      type: "DEL_ITEM",
-      payload: {
-        item,
-        dispatchDeletedTodoIems
-      }
-    }
-    dispatchTodoIems(DeleteItemAction)
-  }
-
+ 
 
 
   return (
     <center className="todo-container">
-      <TodoItemsContext.Provider
-        value={{
-          //since key and value same so can use directly instead of todoItems:todoItems, ...
-          todoItems,
-          handleOnClickDelete,
-          onClickAdd,
-        }}
-      >
+     <TodoItemContextProvider>
         <AppName />
         <AddTodo />
         {/*  <Heading text={"ToDo  List"}/> */}
         <TodoItemsContainer showDelBtn={"true"} />
-      </TodoItemsContext.Provider>
-
-      {/*  <Heading text={"Completed ToDo List"}/> */}
-      <DeletedTodoItemsContext.Provider value={{ deletedTodoItems }}>
         <TodoItemsContainer showDelBtn={"false"} />
-      </DeletedTodoItemsContext.Provider>
+
+      {/*  <Heading text={"Completed ToDo List"}/> 
+       <DeletedTodoItemsContext.Provider value={{ deletedTodoItems }}>
+        
+      </DeletedTodoItemsContext.Provider>*/}
+
+      </TodoItemContextProvider>
+
+
     </center>
   );
 }
--- src/store/todo-items-store.jsx
+++ src/store/todo-items-store.jsx
@@ -1,3 +1,97 @@
-import { createContext } from "react";
+import { useReducer,createContext } from "react";
 
-export const TodoItemsContext=createContext([])
+export const TodoItemsContext = createContext([])
+
+let i = 0
+const todoItemsReducer = (currentTodoItems, action) => {
+  //param 1 of todoItems useReducer
+  let newTodoItems = currentTodoItems;
+
+  if (action.type == "NEW_ITEM") {
+    newTodoItems = [
+      ...currentTodoItems,
+      { name: action.payload.todoName, date: action.payload.date },
+    ];
+  } else if (action.type == "DEL_ITEM") {
+    let item = action.payload.item
+    // console.log(item)
+    if (newTodoItems.includes(item)) {
+      let newItems = [...newTodoItems]; //create new item list
+      let indexOfSelectedItem = newTodoItems.indexOf(item); //get index of selected item
+      let removed_item = newItems.splice(indexOfSelectedItem, 1); //remove the selected item from list and return it
+      // console.log(removed_item[0])
+
+      newTodoItems = newItems
+      console.log(i += 1)
+      action.payload.dispatchDeletedTodoIems({ type: "ADD_ITEM", item: removed_item[0] })
+    }
+
+  }
+
+  return newTodoItems;
+};
+const deletedTodoItemsReducer = (currentState, action) => {
+  let deletedTodoItems = currentState;
+  console.log(deletedTodoItems)
+  if (action.type === "ADD_ITEM") {
+
+    if (!currentState.includes(action.item)) {
+      deletedTodoItems = [...currentState, action.item]
+    }
+  }
+
+  return deletedTodoItems
+
+}
+
+const TodoItemContextProvider = ({children}) => {
+
+  const [todoItems, dispatchTodoIems] = useReducer(todoItemsReducer, []);
+  const [deletedTodoItems, dispatchDeletedTodoIems] = useReducer(deletedTodoItemsReducer, []); //main deleted todo list
+
+  //updaing todoItems main list when click add
+  const onClickAdd = (todoName, date) => {
+    //creating obj for action
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: {
+        todoName,
+        date,
+      },
+    };
+    // calling finc which we get from UseReducer whichtaking obj as arg
+    dispatchTodoIems(newItemAction);
+  };
+
+  const handleOnClickDelete = (event, item) => {
+    const DeleteItemAction = {
+      type: "DEL_ITEM",
+      payload: {
+        item,
+        dispatchDeletedTodoIems
+      }
+    }
+    dispatchTodoIems(DeleteItemAction)
+  }
+
+
+
+
+
+  return (
+    <TodoItemsContext.Provider
+    value={{
+      //since key and value same so can use directly instead of todoItems:todoItems, ...
+      todoItems,
+      handleOnClickDelete,
+      onClickAdd,
+      deletedTodoItems
+    }}
+  >
+   {children}
+   
+  </TodoItemsContext.Provider>
+
+  )
+}
+export default TodoItemContextProvider
